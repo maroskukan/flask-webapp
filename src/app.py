4b7f756f14c53299e2673d38600b301121bf348c
Maros Kukan <maros.kukan@me.com>
2021-06-09 21:04:36 +0200

    Change root view and add basic templates based on bootstrap

--- src/app.py
+++ src/app.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 
 app = Flask(__name__)
+views = 0
 
 @app.route('/')
-def welcome():
-    return "Welcome to my Flash Cards application!"
+def home():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
